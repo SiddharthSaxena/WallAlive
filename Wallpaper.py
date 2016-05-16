d974136b323fcf31cd4a88d33f01e385a677dece
Siddharth Saxena <SiddharthSaxena@users.noreply.github.com>
2016-05-16 22:41:59 +0530

    Create Wallpaper.py

---
+++ Wallpaper.py
@@ -0,0 +1,15 @@
+import glob
+import WallAlive
+
+formats = ['*.jpg', '*.png', '*.bmp', '*.gif', '*.tif', '*.tiff']
+wall = src.WallAlive
+directory = wall.path()
+timer = wall.delay()
+wall.regedit()
+
+# For the images to be displayed indefinitely, the configuration has to be run in an infinite loop.
+while 1:
+    for format in formats:
+        pictureDirectory = glob.glob(directory + format)
+        for picture in pictureDirectory:
+            wall.run(picture, timer)
