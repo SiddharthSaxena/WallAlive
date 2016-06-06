67937a5cb34b45ad17ad7b988bd55446b6f7306f
Siddharth Saxena <SiddharthSaxena@users.noreply.github.com>
2016-06-06 16:36:38 +0530

    Update Wallpaper.py

--- Wallpaper.py
+++ Wallpaper.py
@@ -1,11 +1,17 @@
 import glob
+import os
 import WallAlive
 
+user = os.path.expanduser('~')
 formats = ['*.jpg', '*.png', '*.bmp', '*.gif', '*.tif', '*.tiff']
+
 wall = WallAlive.WallAlive
+temp = os.path.join(user + '\\WallAlive\\WallAlive\\WallAlive.py')
+if not os.path.exists(temp):
+    wall.install()
+wall.regedit()
 directory = wall.path()
 timer = wall.delay()
-wall.regedit()
 
 # For the images to be displayed indefinitely, the configuration has to be run in an infinite loop.
 while 1:
